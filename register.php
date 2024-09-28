99cc0b96b3d87b5a92ae6bf383624e56f9d359db
Moonblee2 <yeisy.gomezpavon@cesunbc.edu.mx>
2024-09-28 16:53:37 -0700

    db, register, vinculacion de styles a Style

--- register.php
+++ register.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
+
+
+
+
+
+
+
+
+
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro - TaskManager</title>
     <link rel="stylesheet" href="Style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp" rel="stylesheet">
 </head>
 <body>
     <div class="login-container">
